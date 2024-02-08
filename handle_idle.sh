630b28f3b67adf821ec62006f29bb9d4916ecf2f
Julius Schumacher <juliusschumacher@gmail.com>
2024-02-08 15:59:59 +0100

    tweaks

--- handle_idle.sh
+++ handle_idle.sh
@@ -4,5 +4,7 @@ swayidle -w \
     timeout 300 'gtklock -m playerctl-module -S -d' \
     timeout 600 'hyprctl dispatch dpms off' \
     resume 'hyprctl dispatch dpms on' \
-    before-sleep 'gtklock -m playerctl-module -S -d' \
-    lock 'gtklock -m playerctl-module -S -d'
+    before-sleep 'waylock' \
+    # before-sleep 'gtklock -m playerctl-module -S -s ~/.config/gtklock/theme.css -d' \
+    lock 'waylock'
+    # lock 'gtklock -m playerctl-module -S -d'
