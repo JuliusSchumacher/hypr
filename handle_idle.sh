a9b74bc08ce510dcfaa62a58a035737d91c0404d
Julius Schumacher <juliusschumacher@gmail.com>
2024-12-02 11:58:15 +0100

    foo

--- handle_idle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-swayidle -w \
-    timeout 300 'gtklock -m playerctl-module -S -s ~/.config/gtklock/theme.css -d' \
-    timeout 600 'hyprctl dispatch dpms off' \
-    resume 'hyprctl dispatch dpms on' \
-    before-sleep 'gtklock -m playerctl-module -S -s ~/.config/gtklock/theme.css -d' \
-    lock 'gtklock -m playerctl-module -S -s ~/.config/gtklock/theme.css -d'
---
+++ screenshot.sh
@@ -0,0 +1,1 @@
+grim -g "$(slurp -o -r -c '#ff0000ff')" - | satty --filename - --fullscreen --output-filename ~/media/photos/screenshots/satty-$(date '+%Y%m%d-%H:%M:%S').png
