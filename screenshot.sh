3ca8f5acfaaa7bb8fe49b1c713170218bc155a9c
Julius Schumacher <juliusschumacher@gmail.com>
2025-09-19 17:18:20 +0200

    Fix some broken configs

--- screenshot.sh
+++ screenshot.sh
@@ -1,1 +1,1 @@
-grim -g "$(slurp -o -r -c '#ff0000ff')" - | satty --filename - --fullscreen --output-filename ~/media/photos/screenshots/satty-$(date '+%Y%m%d-%H:%M:%S').png
+grim -g "$(slurp -o -c '#ff0000ff')" - | swappy -f -
